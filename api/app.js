89a8d49993b283e9eb98d163a0e5c25ef5600821
pichapat saleechanta <pichapat42@gmail.com>
2022-04-26 18:09:51 +0700

    "add vercel"

--- api/app.js
+++ api/app.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
-const router = require("../routes/activity-routes")
+const cors = require("cors");
+const router = require("../routes/activity-routes");
 const app = express();
 
-require('dotenv').config();
-const config = require('../config');
+require("dotenv").config();
+const config = require("../config");
 const PORT = config.port;
 
 //Middlewear
 app.use(express.json());
 app.use(cors());
-app.use("/activities", router); // localhost:5000/activities
-if (config.isVercel) {
-    app.use(async (req, res, next) => {
-      await mongoose.connect(config.mongoUri, config.mongoOptions);
-      return next();
-    });
-  }
 
+if (config.isVercel) {
+  app.use(async (req, res, next) => {
+    await mongoose.connect(config.mongoUri, config.mongoOptions);
+    return next();
+  });
+}
+app.use("/activities", router); // localhost:5000/activities
 module.exports = app;
 
 //luY7ixwSoVK60Eyz
